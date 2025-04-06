846851334849955282de86e2372ca7be93040964
신하란 <74278484+hashin22@users.noreply.github.com>
2025-04-06 15:19:37 +0900

    3

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <ul>
           <li>이메일: hashin22@naver.com</li>
           <li>전화번호: 010-2554-0681</li>
-          <li>GitHub: <a href="https://github.com/hashin22/Resume" target="_blank" rel="noreferrer">github.com/yourID</a></li>
+          <li>GitHub: <a href="https://github.com/hashin22/Resume" target="_blank" rel="noreferrer">github.com/hashin22</a></li>
         </ul>
       </section>
 
